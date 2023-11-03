6773fbed38a7f8835a7e42dd5c1fed776dd2af42
Your Name <you@example.com>
2023-11-03 21:19:02 +0700

    chinh

--- app/src/main/java/com/example/mainactivity/activity/Welcome.java
+++ app/src/main/java/com/example/mainactivity/activity/Welcome.java
@@ -11,6 +11,6 @@ public class Welcome extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity__screen_saver);
+        setContentView(R.layout.welcome);
     }
 }
