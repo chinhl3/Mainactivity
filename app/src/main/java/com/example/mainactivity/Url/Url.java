37e78302466c705b26114a8f05ddebc124848875
Dat <led980853@gmail.com>
2023-11-16 15:49:14 +0700

    name

--- app/src/main/java/com/example/mainactivity/Url/Url.java
+++ app/src/main/java/com/example/mainactivity/Url/Url.java
@@ -1,7 +1,7 @@
 package com.example.mainactivity.Url;
 
 public class Url {
-    public static final String ROOT_URL = "http://172.16.94.79/du_an_1/";
+    public static final String ROOT_URL = "http://172.16.99.247/du_an_1/";
     public static final String GETDATA_SP=ROOT_URL+"get_data_sp.php";
 
 
---
+++ app/src/main/java/com/example/mainactivity/activity/Car.java
@@ -0,0 +1,16 @@
+package com.example.mainactivity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mainactivity.R;
+
+public class Car extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_car);
+    }
+}
